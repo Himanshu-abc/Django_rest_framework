5c3640da5fbb665d73dbb061280c406849df66b2
Himanshu-abc <himanshupatidar663@gmail.com>
2021-09-02 21:33:33 +0530

    Third Commit

--- rest_app1/urls.py
+++ rest_app1/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('article/', views.article_list, name='article_list'),
+    path('detail/<str:pk>', views.article_detail, name='article_detail'),
 
 ]
